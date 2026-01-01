7e67c5ba40ae9a7c729260e7c9c046982e173eca
Christoph Engelbert (noctarius) <me@noctarius.com>
2026-01-01 12:50:29 +0100

    First updates and fixes

--- prototype/external.mk
+++ prototype/external.mk
@@ -1,2 +1,2 @@
-include $(sort $(wildcard $(BR2_EXTERNAL_VELAOS_PATH)/package/*/*.mk))
-include $(sort $(wildcard $(BR2_EXTERNAL_VELAOS_PATH)/linux/*.mk))
+include $(sort $(wildcard $(BR2_EXTERNAL)/package/*/*.mk))
+include $(sort $(wildcard $(BR2_EXTERNAL)/linux/*.mk))
