628cbb23db312558a8a9de1e718db7e10e22bc4d
Christoph Engelbert (noctarius) <me@noctarius.com>
2026-01-02 21:42:03 +0100

    Making sure we rename the kernel image

--- prototype/boards/vela-qemu-x64/scripts/post-image.sh
+++ prototype/boards/vela-qemu-x64/scripts/post-image.sh
@@ -6,4 +6,10 @@ echo -n "Converting RAW image to QCOW2... "
 qemu-img convert -f raw -O qcow2 -o cluster_size=2M,lazy_refcounts=on ${BINARIES_DIR}/rootfs.ext2 ${BINARIES_DIR}/disk.qcow2
 echo "done."
 
+if [ -f ${BINARIES_DIR}/bzImage ]; then
+  echo "Symlinking Linux kernel image..."
+  [ -f ${BINARIES_DIR}/vmlinuz ] && rm ${BINARIES_DIR}/vmlinuz
+  ln -s bzImage ${BINARIES_DIR}/vmlinuz
+fi
+
 echo "Build completed."
