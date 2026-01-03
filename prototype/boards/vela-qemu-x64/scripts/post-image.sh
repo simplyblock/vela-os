ef91f069c5cf49848b82cf998cfeeb9b9e2e2f23
Christoph Engelbert (noctarius) <me@noctarius.com>
2026-01-03 10:43:05 +0100

    Added docker image build step after creating disk image

--- prototype/boards/vela-qemu-x64/scripts/post-image.sh
+++ prototype/boards/vela-qemu-x64/scripts/post-image.sh
@@ -4,6 +4,7 @@ BOARD_DIR="$( dirname "${0}" )"
 
 echo -n "Converting RAW image to QCOW2... "
 qemu-img convert -f raw -O qcow2 -o cluster_size=2M,lazy_refcounts=on ${BINARIES_DIR}/rootfs.ext2 ${BINARIES_DIR}/disk.qcow2
+rm -rf ${BINARIES_DIR}/rootfs.ext2
 echo "done."
 
 if [ -f ${BINARIES_DIR}/bzImage ]; then
@@ -11,5 +12,9 @@ if [ -f ${BINARIES_DIR}/bzImage ]; then
   [ -f ${BINARIES_DIR}/vmlinuz ] && rm ${BINARIES_DIR}/vmlinuz
   ln -s bzImage ${BINARIES_DIR}/vmlinuz
 fi
+rm -rf ${BINARIES_DIR}/vmlinux
+
+echo "Building container image wrapper..."
+docker build -t vela-image -f "${BR2_EXTERNAL_VELAOS_PATH}/boards/vela-qemu-x64/scripts/Dockerfile" "${BINARIES_DIR}"
 
 echo "Build completed."
