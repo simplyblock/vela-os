dc75d168b01c87f23ad664387eeb6f9aba2584f4
Christoph Engelbert (noctarius) <me@noctarius.com>
2026-01-04 21:56:37 +0100

    Added hash to vm-monitor

--- prototype/package/vm-monitor/vm-monitor.mk
+++ prototype/package/vm-monitor/vm-monitor.mk
@@ -1,6 +1,6 @@
 VM_MONITOR_VERSION = release-proxy-8853
 VM_MONITOR_SOURCE = $(VM_MONITOR_VERSION).tar.gz
-VM_MONITOR_SITE =  https://github.com/neondatabase/neon/archive/refs/tags
+VM_MONITOR_SITE = https://github.com/neondatabase/neon/archive/refs/tags
 VM_MONITOR_LICENSE = Apache2.0
 VM_MONITOR_SUBDIR = libs/vm_monitor
 
