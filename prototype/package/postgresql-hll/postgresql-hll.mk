eed52bdacce39592871079e3c72f1b6f88a0207e
Christoph Engelbert (noctarius) <me@noctarius.com>
2026-01-08 15:41:35 +0100

    Added postgresql-hll extension

--- prototype/package/postgresql-hll/postgresql-hll.mk
+++ prototype/package/postgresql-hll/postgresql-hll.mk
@@ -2,19 +2,33 @@ POSTGRESQL_HLL_VERSION = 2.19
 POSTGRESQL_HLL_SOURCE = v$(POSTGRESQL_HLL_VERSION).tar.gz
 POSTGRESQL_HLL_SITE = https://github.com/citusdata/postgresql-hll/archive/refs/tags
 POSTGRESQL_HLL_LICENSE = PostgreSQL
-POSTGRESQL_HLL_CONFIG_SCRIPTS = pg_config
-POSTGRESQL_HLL_LDFLAGS = $(TARGET_LDFLAGS) $(TARGET_NLS_LIBS)
-POSTGRESQL_HLL_DEPENDENCIES = postgresql-18
+POSTGRESQL_HLL_DEPENDENCIES = postgresql libpqxx
 POSTGRESQL_HLL_SUPPORTS_IN_SOURCE_BUILD = NO
+POSTGRESQL_HLL_INSTALL_STAGING = YES
 
+# PGXS build: use the pg_config from staging so it picks the right include/lib dirs
 define POSTGRESQL_HLL_BUILD_CMDS
-	cd $(@D) && $(MAKE1) PG_CONFIG=$(STAGING_DIR)/usr/bin/pg_config USE_PGXS=1 $(TARGET_CONFIGURE_OPTS) PKGCONFIG="$(PKG_CONFIG_HOST_BINARY)" OPTFLAGS="" -C $(@D)
+	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D) USE_PGXS=1 \
+		CPP="$(TARGET_CXX)" \
+		CXX="$(TARGET_CXX)" \
+		CC="$(TARGET_CC)" \
+		GCC="$(TARGET_CC)" \
+		LD="$(TARGET_LD)" \
+		PG_CONFIG=$(STAGING_DIR)/usr/bin/pg_config
 endef
 
+# Install into target rootfs. PGXS honors DESTDIR.
 define POSTGRESQL_HLL_INSTALL_TARGET_CMDS
-	$(INSTALL) $(@D)/prefix.so $(TARGET_DIR)/usr/lib/postgresql
-	$(INSTALL) $(@D)/prefix.control $(TARGET_DIR)/usr/share/postgresql/extension
-	$(INSTALL) $(@D)/*.sql $(TARGET_DIR)/usr/share/postgresql/extension
+	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D) USE_PGXS=1 \
+		PG_CONFIG=$(STAGING_DIR)/usr/bin/pg_config \
+		DESTDIR=$(TARGET_DIR) install
+endef
+
+# Optional: install into staging too (useful if other target packages need it at build time)
+define POSTGRESQL_HLL_INSTALL_STAGING_CMDS
+	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D) USE_PGXS=1 \
+		PG_CONFIG=$(STAGING_DIR)/usr/bin/pg_config \
+		DESTDIR=$(STAGING_DIR) install
 endef
 
 $(eval $(generic-package))
