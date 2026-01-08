d7a50ed2b75ef4ce173c1ed45a74722946f773f0
Christoph Engelbert (noctarius) <me@noctarius.com>
2026-01-08 15:36:04 +0100

    Added rum extension

--- prototype/package/rum/rum.mk
+++ prototype/package/rum/rum.mk
@@ -2,13 +2,12 @@ RUM_VERSION = 1.3.15
 RUM_SOURCE = $(RUM_VERSION).tar.gz
 RUM_SITE = https://github.com/postgrespro/rum/archive/refs/tags
 RUM_LICENSE = PostgreSQL
-RUM_CONFIG_SCRIPTS = pg_config
 RUM_LDFLAGS = $(TARGET_LDFLAGS) $(TARGET_NLS_LIBS)
-RUM_DEPENDENCIES = postgresql-18 boost custom-postgis
+RUM_DEPENDENCIES = postgresql
 RUM_SUPPORTS_IN_SOURCE_BUILD = NO
 
 define RUM_BUILD_CMDS
-	cd $(@D) && $(MAKE1) USE_PGXS=1 PG_CONFIG=$(STAGING_DIR)/usr/bin/pg_config $(TARGET_CONFIGURE_OPTS) PKGCONFIG="$(PKG_CONFIG_HOST_BINARY)" OPTFLAGS="" -C $(@D)
+	cd $(@D) && $(MAKE1) PG_CONFIG=$(STAGING_DIR)/usr/bin/pg_config USE_PGXS=1 PG_CONFIG=$(STAGING_DIR)/usr/bin/pg_config $(TARGET_CONFIGURE_OPTS) PKGCONFIG="$(PKG_CONFIG_HOST_BINARY)" OPTFLAGS="" -C $(@D)
 endef
 
 define RUM_INSTALL_TARGET_CMDS
