7bc6d1ec1b924f36f5bd831945f5ff91d9dd29b7
Christoph Engelbert (noctarius) <me@noctarius.com>
2026-01-13 22:05:05 +0100

    Fixed pgrouting installation

--- prototype/package/pgrouting/pgrouting.mk
+++ prototype/package/pgrouting/pgrouting.mk
@@ -6,4 +6,17 @@ PGROUTING_LDFLAGS = $(TARGET_LDFLAGS) $(TARGET_NLS_LIBS)
 PGROUTING_DEPENDENCIES = postgresql boost
 PGROUTING_SUPPORTS_IN_SOURCE_BUILD = NO
 
+define PGROUTING_PATH_FIXUP
+	for f in $(TARGET_DIR)/usr/lib/postgresql/18/lib/libpgrouting*; do \
+		mv "$$f" "$(TARGET_DIR)/usr/lib/postgresql/$$(basename "$$f")"; \
+	done
+	for f in $(TARGET_DIR)/usr/share/postgresql/18/extension/pgrouting*; do \
+		mv "$$f" "$(TARGET_DIR)/usr/share/postgresql/extension/$$(basename "$$f")"; \
+	done
+	rm -rf $(TARGET_DIR)/usr/lib/postgresql/18
+	rm -rf $(TARGET_DIR)/usr/share/postgresql/18
+endef
+
+PGROUTING_POST_INSTALL_TARGET_HOOKS += PGROUTING_PATH_FIXUP
+
 $(eval $(cmake-package))
