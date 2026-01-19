97a7d61ab67d90eba3fb0a53bac92e2e1d3a7ad3
Max Schettler <max@simplyblock.io>
2026-01-19 18:58:44 +0100

    Add vector service

--- prototype/package/vector/vector.mk
+++ prototype/package/vector/vector.mk
@@ -16,7 +16,15 @@ endef
 define VECTOR_INSTALL_TARGET_CMDS
 	mkdir -p $(TARGET_DIR)/sbin
 	$(INSTALL) $(@D)/vector-$(VECTOR_TARGET_ARCH)-unknown-linux-gnu/bin/vector $(TARGET_DIR)/sbin/vector
-	$(INSTALL) -m 0755 $(VECTOR_PKGDIR)/vector-server $(TARGET_DIR)/usr/sbin
+endef
+
+define VECTOR_INSTALL_INIT_SYSTEMD
+	$(INSTALL) -D -m 644 $(VECTOR_PKGDIR)/vector.service \
+		$(TARGET_DIR)/usr/lib/systemd/system/vector.service
+endef
+
+define VECTOR_USERS
+	vector -1 vector -1 * - - - vector
 endef
 
 $(eval $(generic-package))
