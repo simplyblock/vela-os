14f96f0c715e61e30fa417842fdb01ed1a333ac1
Max Schettler <max@simplyblock.io>
2026-01-19 18:40:52 +0100

    Add vm-monitor service

--- prototype/package/vm-monitor/vm-monitor.mk
+++ prototype/package/vm-monitor/vm-monitor.mk
@@ -4,4 +4,13 @@ VM_MONITOR_SITE = https://github.com/neondatabase/neon/archive/refs/tags
 VM_MONITOR_LICENSE = Apache2.0
 VM_MONITOR_SUBDIR = libs/vm_monitor
 
+define VM_MONITOR_INSTALL_INIT_SYSTEMD
+	$(INSTALL) -D -m 644 $(VM_MONITOR_PKGDIR)/vm-monitor.service \
+		$(TARGET_DIR)/usr/lib/systemd/system/vm-monitor.service
+endef
+
+define VM_MONITOR_USERS
+	vm-monitor -1 vm-monitor -1 - - - - vm-monitor
+endef
+
 $(eval $(cargo-package))
