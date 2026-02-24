fe8dc85f7f33ca2035ce2f7f86e9139366519b61
Manohar Reddy <manohar@simplyblock.io>
2026-02-24 21:45:04 +0530

    pitr restore

---
+++ prototype/boards/qemu-common/rootfs_overlay/usr/local/bin/postgres-startup.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+set -e
+PGDATA=/var/lib/postgresql/data
+
+ulimit -c unlimited
+
+PITR_ARGS=""
+
+if [ -n "$RECOVERY_TARGET_TIME" ] && [ ! -f "$PGDATA/.recovery_done" ]; then
+    echo "Configuring Point-in-Time Recovery to $RECOVERY_TARGET_TIME"
+    
+    PITR_ARGS="-c snapshot_pitr_target_time='$RECOVERY_TARGET_TIME'"
+    
+    # Mark recovery as configured so we don't do it again on next reboot
+    touch "$PGDATA/.recovery_done"
+fi
+
+pg_ctl start -o "-c config_file=/vela/config/postgresql.conf -c hba_file=/vela/config/pg_hba.conf $PITR_ARGS" -l /tmp/service-postgresql.fifo
