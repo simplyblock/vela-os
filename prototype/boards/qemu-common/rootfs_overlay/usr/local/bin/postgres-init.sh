29c3c691181314effff592ee60807e9fade945ba
Manohar Reddy <manohar@simplyblock.io>
2026-03-17 19:55:54 +0530

    use nested mount at /var/lib/postgresql

--- prototype/boards/qemu-common/rootfs_overlay/usr/local/bin/postgres-init.sh
+++ prototype/boards/qemu-common/rootfs_overlay/usr/local/bin/postgres-init.sh
@@ -2,21 +2,13 @@
 set -e
 
 PGDATA=/var/lib/postgresql/data
-WAL_MOUNT=/mnt/postgres_wal
-PGWAL=$WAL_MOUNT/wal
+WAL_MOUNT=/var/lib/postgresql/wal
+PGWAL=$WAL_MOUNT/pg_wal
 
 # Basic directories
-mkdir -p "$PGDATA" /run/postgresql
-chown -R postgres:postgres "$PGDATA" /run/postgresql
-
-INIT_OPTS="--username=vela"
-
-# If external WAL volume is present, initialize its directory and append --waldir
-if [ -d "$WAL_MOUNT" ]; then
-    mkdir -p "$PGWAL"
-    chown -R postgres:postgres "$WAL_MOUNT" "$PGWAL"
-    INIT_OPTS="$INIT_OPTS --waldir=$PGWAL"
-fi
+mkdir -p "$PGDATA" /run/postgresql "$PGWAL"
+chown -R postgres:postgres "$PGDATA" /run/postgresql "$WAL_MOUNT" "$PGWAL"
+INIT_OPTS="--username=vela --waldir=$PGWAL"
 
 # Initialize the Postgres database if not already initialized
 if [ ! -s "$PGDATA/PG_VERSION" ]; then
